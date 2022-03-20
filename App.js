e10dc5bfd885fcd33f126bfa80b5633b46e80877
yassineelhoubi <elhoubiyassine@gmail.com>
2022-03-20 22:28:17 +0100

    fetch data with graphQl

--- App.js
+++ App.js
@@ -1,10 +1,74 @@
 import express from 'express';
 import bodyParser from "body-parser";
+import userData from './MOCK_DATA.json'
+import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from 'graphql';
+import { graphqlHTTP } from 'express-graphql';
+
 const app = express();
 app.use(bodyParser.json());
-app.get("/",(req,res) => {
-    res.send("hello world!");
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields: () => ({
+        id: { type: GraphQLInt },
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        gender: { type: GraphQLString },
+
+    })
 })
-app.listen(5000,() => {
-     console.log(`app is listening to port 5000`);
+
+
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return userData
+            }
+        }
+    }
+});
+const Mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        createUser: {
+            type: UserType,
+            args: {
+                firstName: { type: GraphQLString },
+                lastName: { type: GraphQLString },
+                email: { type: GraphQLString },
+                gender: { type: GraphQLString },
+            },
+            resolve(parent, args) {
+                userData.push({
+                    id: userData.length + 1,
+                    firstName: args.firstName,
+                    lastName: args.lastName,
+                    email: args.email,
+                    gender: args.gender
+                });
+                return args
+            }
+        }
+    }
+});
+
+
+
+const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(5000, () => {
+    console.log(`app is listening to port 5000`);
 })
